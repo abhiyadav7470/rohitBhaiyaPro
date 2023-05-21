b2409c88bcec98580f0e4cee487293dcc9d3e351
abhiyadav7470 <108442826+abhiyadav7470@users.noreply.github.com>
2023-05-21 23:44:00 +0530

    updates

--- GYMWEBPAGE/back-end/models/register.schema.js
+++ GYMWEBPAGE/back-end/models/register.schema.js
@@ -2,16 +2,31 @@ const mongoose = require('mongoose');
 // const Schema = mongoose.Schema();
 
 var registerschema = new mongoose.Schema({
+    firstName:{
+        type: String
+    },
+    middleName:{
+        type: String
+    },
+    lastName:{
+        type: String
+    },
+    gender:{
+        type: String
+    },
+    mobilno:{
+        type: Number
+    },
+    trainings:{
+        type: String
+    },
+    state:{
+        type: String
+    },
     email:{
         type: String
     },
     password:{
-        type: String
-    },
-    name:{
-        type: String
-    },
-    trainer:{
         type: String
     }
 },{
